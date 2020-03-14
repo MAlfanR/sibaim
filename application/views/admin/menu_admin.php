998cf4be6a5f2c30559791093bcbb714f0191381
nardiyansah <janatri1398@gmail.com>
2020-03-14 16:15:06 +0700

    Update menu_admin.php
    - menu jadi ada 3
    - pojok kanan atas nama & gambar administrator

--- application/views/admin/menu_admin.php
+++ application/views/admin/menu_admin.php
@@ -16,71 +16,91 @@
 
 <body>
 
-  <div class="container">
+<div class="container-fluid">
 
-    <div class="text-center p-4">
-      <!-- logo -->
-      <img src="assets/images/logoBaim.png" class="mx-auto d-block" alt="" width="80px">
-      <h1>SIBAIM</h1>
-      <h6>Sistem Informasi Masjid Baitul Ilmi</h6>
+    <div class="row">
+
+      <div class="col-lg-2">
+
+      </div>
+
+      <div class="col-lg-8">
+        <div class="text-center p-4">
+          <!-- logo -->
+          <img src="../assets/images/logoBaim.png" class="mx-auto d-block" alt="" width="80px">
+          <h1>SIBAIM</h1>
+          <h6>Sistem Informasi Masjid Baitul Ilmi</h6>
+        </div>
+      </div>
+
+      <div class="col-lg-2">
+        <div class="row mt-3">
+          <div class="col">
+            <div class="dropdown">
+              <button type="button" class="btn dropdown-toggle" id="dropdownMenuButton" data-toggle="dropdown"
+                aria-haspopup="true" aria-expanded="false">Administrator
+              </button>
+              <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                <a class="dropdown-item" href="#">Profile</a>
+                <a class="dropdown-item" href="#">Sign-out</a>
+              </div>
+            </div>
+          </div>
+          <div class="col text-center">
+            <p><img src="../assets/images/administrator.png" class="w-100" alt=""></p>
+          </div>
+        </div>
+      </div>
     </div>
 
-    <!-- menu admin -->
-    <div class="row justify-content-center">
 
-      <div class="col-lg-5">
+    <!-- menu admin -->
+    <div class="row justify-content-center mt-5">
+
+      <div class="col-lg">
+        <a href="#">
+          <div class="card">
+            <h5 class="card-title text-center text-white">Update informasi barang hilang</h5>
+            <div class="card-body text-center">
+              <img src="../assets/images/ikon_barang_hilang.png" alt="menu informasi barang hilang" class="w-25">
+            </div>
+          </div>
+        </a>
+      </div>
+
+      <div class="col-lg">
         <a href="#">
           <div class="card">
 
             <h5 class="card-title text-center text-white">Update Donasi</h5>
             <div class="card-body text-center">
-              <img src="assets/images/ikon_menu.png" alt="menu update donasi" class="w-25">
+              <img src="../assets/images/ikon_update_donasi.png" alt="menu update donasi" class="w-25">
             </div>
 
           </div>
         </a>
       </div>
 
-      <div class="col-lg-5">
+      <div class="col-lg">
         <a href="#">
           <div class="card">
             <h5 class="card-title text-center text-white">Update Peminjaman inventori masjid</h5>
             <div class="card-body text-center">
-              <img src="assets/images/ikon_menu.png" alt="menu peminjaman inventori" class="w-25">
+              <img src="../assets/images/ikon_inventori_masjid.png" alt="menu peminjaman inventori" class="w-25">
             </div>
           </div>
         </a>
       </div>
 
-    </div>
-    <div class="row justify-content-center">
-
-      <div class="col-lg-5">
-        <a href="#">
-          <div class="card">
-            <h5 class="card-title text-center text-white">Update informasi barang hilang</h5>
-            <div class="card-body text-center">
-              <img src="assets/images/ikon_menu.png" alt="menu informasi barang hilang" class="w-25">
-            </div>
-          </div>
-        </a>
-      </div>
-
-      <div class="col-lg-5">
-        <a href="#">
-          <div class="card">
-            <h5 class="card-title text-center text-white">Update Konten</h5>
-            <div class="card-body text-center">
-              <img src="assets/images/ikon_menu.png" alt="menu konten" class="w-25">
-            </div>
-          </div>
-        </a>
-      </div>
     </div>
 
   </div>
 
   <!-- bootstrap js -->
+  <script src="assets/js/jquery-3.2.1.slim.min.js">
+  </script>
+  <script src="assets/js/popper.min.js">
+  </script>
   <script src="assets/js/bootstrap.bundle.min.js"></script>
 </body>
 
