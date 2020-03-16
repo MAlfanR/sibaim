7690585eedc16fad0e25c296ca0e6538775144b7
nardiyansah <janatri1398@gmail.com>
2020-03-16 20:15:48 +0700

    Update login_admin.php
    - tambah popper dan jquery

--- application/views/admin/login_admin.php
+++ application/views/admin/login_admin.php
@@ -52,8 +52,13 @@
     <!-- end card login -->
   </div>
 
-  <!-- bootstrap js -->
-  <script src="../assets/js/bootstrap.bundle.min.js"></script>
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+	<script src="assets/js/jquery-3.2.1.slim.min.js">
+	</script>
+	<script src="assets/js/popper.min.js">
+	</script>
+	<script src="assets/js/bootstrap.min.js">
+	</script>
 </body>
 
 </html>
